73f39b7cd4e6798a0b83236ed2e1d7c0d23b048b
bangmijung <66621920+bangmijung@users.noreply.github.com>
2023-10-10 23:44:51 +0900

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -3,3 +3,12 @@ import streamlit as st
 st.title("Test")
 lst = ["1","2","6"]
 st.multiselect("label",lst,lst)
+keywords = st_tags(
+    label='# Enter Keywords:',
+    text='Press enter to add more',
+    value=['Zero', 'One', 'Two'],
+    suggestions=['five', 'six', 'seven', 
+                 'eight', 'nine', 'three', 
+                 'eleven', 'ten', 'four'],
+    maxtags = 4,
+    key='1')
